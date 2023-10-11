4783ed716758b962afae9ebcebe66899962a86f7
jpf <juanpablofavale@hotmail.com>
2023-10-11 17:19:49 -0300

    agregando redes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
 import './App.css'
 import Atropos from 'atropos/react';
-import { FaSquareXTwitter, FaSquareFacebook, FaSquareInstagram, FaSquareWhatsapp, FaPhoneVolume, FaSquarePhone } from "react-icons/fa6";
-import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { ImFacebook2, ImMail } from "react-icons/im";
+import { BiLogoFacebook, BiLogoInstagram, BiLogoWhatsapp, BiMailSend, BiPhoneCall } from "react-icons/bi";
 
 export default function App () {
   return (
@@ -12,36 +10,45 @@ export default function App () {
         className='my-atropos'
         activeOffset={50}
         shadow={false}
-        onEnter={() => console.log('Enter')}
-        onLeave={() => console.log('Leave')}
         onRotate={(x, y) => console.log('Rotate', x, y)}
       >
-        <div data-atropos-offset="5" className="contenido">
-          <img className='logoppal' src="./Nomada.png" alt="nomada img" />
-          <p>VIAJES Y TURISMO</p>
-          <p>PV-2022-139123091-APN-DRAV#MTYD</p>
-          <h1>Próximamente...</h1>
+        <div className="contenido">
+          <img data-atropos-offset="6" className='logoppal' src="./Nomada.png" alt="nomada img" />
+          <p data-atropos-offset="5" className='legajo'>PV-2022-139123091-APN-DRAV#MTYD</p>
+          <p data-atropos-offset="5">VIAJES Y TURISMO</p>
+          <h1 data-atropos-offset="4">Próximamente...</h1>
         </div>
         {/* ... */}
       </Atropos>
       <footer>
         <nav>
           <a href="https://www.facebook.com/nomadaviajesyturismo/" target='_blank' rel='noopener noreferrer'>
-            <p className='face' onClick={() => toast.dark("Próximamente!")}><ImFacebook2 /></p>
+            <p className='face'>
+              <BiLogoFacebook />
+            </p>
           </a>
           <a href="https://www.instagram.com/nomada.evt/" target='_blank' rel='noopener noreferrer'>
-            <p className='insta' onClick={() => toast.dark("Próximamente!")}><FaSquareInstagram /></p>
+            <p className='insta'>
+              <BiLogoInstagram />
+            </p>
           </a>
-          <a href="https://www.facebook.com/nomadaviajesyturismo/" target='_blank' rel='noopener noreferrer'>
-            <p className='twit' onClick={() => toast.dark("Próximamente!")}><FaSquareWhatsapp /></p>
+          <a href="https://wa.me/+542916442676" target='_blank' rel='noopener noreferrer'>
+            <p className='whats'>
+              <BiLogoWhatsapp />
+            </p>
           </a>
-          <a href="https://www.facebook.com/nomadaviajesyturismo/" target='_blank' rel='noopener noreferrer'>
-            <p className='twit' onClick={() => toast.dark("Próximamente!")}><FaSquarePhone /></p>
+          <a href="tel:+542916442676" target='_blank' rel='noopener noreferrer'>
+            <p className='phone'>
+              <BiPhoneCall />
+            </p>
           </a>
-          <a href="https://www.facebook.com/nomadaviajesyturismo/" target='_blank' rel='noopener noreferrer'>
-            <p className='twit' onClick={() => toast.dark("Próximamente!")}><ImMail /></p>
+          <a href="mailto:nomada.evt@gmail.com" target='_blank' rel='noopener noreferrer'>
+            <p className='mail'>
+              <BiMailSend />
+            </p>
           </a>
         </nav>
+        <a target='_blank' rel='noopener noreferrer' href="https://tramitesadistancia.gob.ar/tramitesadistancia/detalle-tipo?id=624">📄Denuncia contra una agencia</a>
       </footer>
     </div>  )
 }
